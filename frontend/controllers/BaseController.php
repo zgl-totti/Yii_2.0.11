329d11ce1940e1f433229190930b21be22f16a08
zgl <724249769@qq.com>
2019-07-11 22:12:18 +0800

    蜘蛛存入

--- frontend/controllers/BaseController.php
+++ frontend/controllers/BaseController.php
@@ -1,18 +1,26 @@
 <?php
 namespace frontend\controllers;
 
+use App\Services\Spider;
 use backend\models\Member;
 use yii\web\Controller;
 
-class BaseController extends Controller{
+class BaseController extends Controller
+{
     public $mid;
-    public function init(){
+
+    public function init()
+    {
         parent::init();
-        //$mid=\Yii::$app->session->get('mid');
-        $mid=47;
-        $info=Member::findOne($mid);
-        \Yii::$app->view->params['info']=$info;
-        \Yii::$app->view->params['keywords']='';
-        $this->mid=$mid;
+
+        //蜘蛛存入
+        Spider::getInstance()->store();
+
+        $mid=\Yii::$app->session->get('mid',47);
+        $info = Member::findOne($mid);
+
+        \Yii::$app->view->params['info'] = $info;
+        \Yii::$app->view->params['keywords'] = '';
+        $this->mid = $mid;
     }
 }
